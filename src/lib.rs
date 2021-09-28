2ee47f2a1e4d6d526ebaa270a41d8f6d1d88e483
jerry73204 <jerry73204@gmail.com>
2021-09-28 13:15:56 +0800

    Define types to serialize/deserialize XML files

---
+++ src/lib.rs
@@ -0,0 +1,112 @@
+use anyhow::Result;
+use noisy_float::prelude::*;
+use serde::{de::Error as _, ser::Error as _, Deserialize, Deserializer, Serialize, Serializer};
+use std::path::PathBuf;
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
+#[serde(rename = "dataset")]
+pub struct Dataset {
+    #[serde(rename = "$value")]
+    pub frames: Vec<Frame>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
+#[serde(rename = "frame")]
+pub struct Frame {
+    pub number: usize,
+    pub file: PathBuf,
+    pub utc: String,
+    pub color: Option<String>,
+    pub depth: Option<String>,
+    pub gsd: R64,
+    pub x: R64,
+    pub y: R64,
+    pub lat: R64,
+    pub lon: R64,
+    #[serde(with = "serde_zero_one_bool")]
+    pub sunny: bool,
+    #[serde(rename = "$value")]
+    pub object_list: ObjectList,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
+#[serde(rename = "objectlist")]
+pub struct ObjectList {
+    #[serde(rename = "$value")]
+    pub objects: Vec<Object>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
+#[serde(rename = "object")]
+pub struct Object {
+    pub id: usize,
+    pub r#box: Box,
+    pub representation: Representation,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
+#[serde(rename = "box")]
+pub struct Box {
+    pub xc: R64,
+    pub yc: R64,
+    pub w: R64,
+    pub h: R64,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
+#[serde(rename = "representation")]
+pub struct Representation {
+    pub r#type: String,
+    pub xc: R64,
+    pub yc: R64,
+    pub w: R64,
+    pub h: R64,
+    pub o: R64,
+}
+
+mod serde_zero_one_bool {
+    use super::*;
+
+    pub fn serialize<S>(value: &bool, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        if *value { "1" } else { "0" }.serialize(serializer)
+    }
+
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<bool, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let text = String::deserialize(deserializer)?;
+        let value = match &*text {
+            "0" => false,
+            "1" => true,
+            text => {
+                return Err(D::Error::custom(format!(
+                    r#"expect "0" or "1", but get "{}""#,
+                    text
+                )))
+            }
+        };
+        Ok(value)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() -> Result<()> {
+        let dir = "/mnt/bbf98471-db16-4e2e-99df-ccd245253072/kit-ais-dataset/Training Data Set/StuttgartCrossroad01";
+
+        for path in glob::glob(&format!("{}/*.xml", dir))? {
+            let path = path?;
+            let text = std::fs::read_to_string(path)?;
+            let dataset: Dataset = serde_xml_rs::from_str(&text)?;
+        }
+
+        Ok(())
+    }
+}
